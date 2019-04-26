a9db9426aec3ea1025172e04043bda7ff266dcb0
esteban201483 <Esteban201483@gmail.com>
2019-04-25 23:23:57 -0600

    Feat: Agrega anotaciones Entity y Table

---
+++ src/main/java/ci1322/Tarea1/Annotations/Entity.java
@@ -0,0 +1,11 @@
+package ci1322.Tarea1.Annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Entity {
+}
